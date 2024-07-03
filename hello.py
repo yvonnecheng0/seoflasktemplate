d564356845a3dfc9cfdff766b48db860bc93c90c
Yvonne <yvonnecheng@MacBook-Air-39.local>
2024-07-04 03:54:24 +0800

    setting up auto deployment

--- hello.py
+++ hello.py
@@ -1,6 +1,7 @@
-from flask import Flask, render_template, url_for, flash, redirect
+from flask import Flask, render_template, url_for, flash, redirect, request
 from forms import RegistrationForm
 from flask_behind_proxy import FlaskBehindProxy
+import git
 
 
 app = Flask(__name__)
@@ -24,5 +25,16 @@ def register():
         flash(f'Account created for {form.username.data}!', 'success')
         return redirect(url_for('home')) # if so - send to home page
     return render_template('register.html', title='Register', form=form)
+
+
+@app.route("/update_server", methods=['POST'])
+def webhook():
+    if request.method == 'POST':
+        repo = git.Repo('/home/ychengproj2/seoflasktemplate')
+        origin = repo.remotes.origin
+        origin.pull()
+        return 'Updated PythonAnywhere successfully', 200
+    else:
+        return 'Wrong event type', 400
 if __name__ == '__main__':
     app.run(debug=True, host="0.0.0.0")
